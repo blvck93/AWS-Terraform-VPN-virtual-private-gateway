1819d03f5e8e892ef578120dc716bdd5b14a21ef
blvck93 <106159599+blvck93@users.noreply.github.com>
2023-10-15 12:03:29 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,8 +7,3 @@ output "vpn2_peer_ip" {
   value       = aws_vpn_connection.lab-vpn-1.tunnel2_address
   description = "Public IP of tunnel 2"
 }
-
-output "ec2_ip" {
-  value       = aws_instance.lab-ec2-private.private_ip
-  description = "Private IP of EC2 instance"
-}
